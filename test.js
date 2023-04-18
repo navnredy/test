1274cd1b23cbad727f17beb742a29bb5adc0afec
NaveenReddy Bommu <bnavnredy@gmail.com>
2023-04-19 04:07:46 +0530

    Initial commit
    Initial commit

---
+++ test.js
@@ -0,0 +1,26 @@
+const conf = {
+    "appSource": "10763",
+    "appName": "5P58537909",
+    "userId": "mCnA2t2yqIE",
+    "password": "couWevSnTSt",
+    "userKey": "cCHa1jZgFfgrqCclMqAXUfP2tssQZBeL",
+    "encryptionKey": "cp8VJkVlczT2lZuErdoZCXBwWegcrmKC"
+}
+
+const { FivePaisaClient } = require("5paisajs");
+var client = new FivePaisaClient(conf);
+/*client.getAccessToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjU4NTM3OTA5Iiwicm9sZSI6ImNDSGExalpnRmZncnFDY2xNcUFYVWZQMnRzc1FaQmVMIiwibmJmIjoxNjgxODM5ODI3LCJleHAiOjE2ODE4Mzk4NTcsImlhdCI6MTY4MTgzOTgyN30.OuXxZvoO2dE3oFLlO_N_RadVbaiDIuzkTN0YhG0TAN0").then(response => {
+    console.log('inside access token');
+});*/
+
+/*client.login("naveenreddy@hotmail.co.in", "F4r3b8@6FPC", "19890930").then((response) => {
+    console.log("logged in!");
+    client.init(response).then(() => {
+        // Fetch holdings, positions or place orders here.
+        // Some things to try out are given below.
+        console.log("logged in!");
+    })
+}).catch((err) =>{
+    // Oh no :/
+   // console.log(err)
+})*/
